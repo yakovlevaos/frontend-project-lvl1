4714d2b1fdbf2db78d2e9163da3a07458a9d293b
Olga <yurkovaos@gmail.com>
2022-05-11 22:39:14 +0700

    .

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
 export var userName = readlineSync.question('May I have your name?');
 console.log(`Hello ${userName}!`);
