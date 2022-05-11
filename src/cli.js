89a4d8775e3c49f2c733c119418fb346dc93c57f
Olga <yurkovaos@gmail.com>
2022-05-11 22:41:49 +0700

    .

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export var userName = readlineSync.question('May I have your name?');
+const userName = readlineSync.question('May I have your name?');
 console.log(`Hello ${userName}!`);
+
+export default userName;
