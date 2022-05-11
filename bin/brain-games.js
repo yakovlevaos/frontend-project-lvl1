87b441808be40f57bcd1318c2a8936067917c6d5
Olga <yurkovaos@gmail.com>
2022-05-11 22:38:45 +0700

    .

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
-import { userName } from '../src/cli.js';
-userName();
+import userName from '../src/cli.js';
+
+const brainGame = () => {
+    console.log('Welcome to the Brain Games!');
+    userName();
+};
+
+export default brainGame;
